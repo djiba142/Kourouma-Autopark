e540b7f0127fdf4d3e50a187ecd6ea4a6723ba4f
Djiba Kourouma <djibakourouma210@gmail.com>
2026-05-15 13:22:50 +0000

    fix: corriger ALLOWED_HOSTS pour corriger l'erreur 400 sur Render

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'bsg-web-dev-secret-change-in-prod-xyz987')
 DEBUG      = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com,*').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
